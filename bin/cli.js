0ee51b026a6164eaee59e24ea9adad13358f2a43
k-garrido <katherinne.g@hotmail.com>
2021-10-27 10:21:18 -0300

    probando cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,26 +1,25 @@
 const { program } = require('commander');
 const { stringify, parse } = require('../index');
-const options = program.opts();
 
 program.version('0.0.1', '-v, --version', 'It show the current version');
 
 // Parse
 program
   .command('parse')
-  .argument('<number>','Integer argument')
-  .description('Convert from Arabic to Roman number, it receives a Number as argument.')
-  .action((number) => {
-      const response =  parse(number)
+  .argument('<string>','Integer argument')
+  .description('Convert from Roman to Arabic number, it receives a String as argument.')
+  .action((string) => {
+      const response =  parse(string)
       console.log(response); 
   })
 
 // Stringify
 program
   .command('stringify')
-  .argument('<string>','String argument')
-  .description('Convert from Roman to Arabic number, it receives a String as argument.')
-  .action((string) => {
-      const toNumber = parseInt(string,10);
+  .argument('<number>','String argument')
+  .description('Convert from Arabic to Roman number, it receives a Number as argument.')
+  .action((number) => {
+      const toNumber = parseInt(number,10);
       const response =  stringify(toNumber);
       console.log(response); 
   })
@@ -28,3 +27,5 @@ program
 
 program.parse(process.argv);
 
+
+process.stdout.write ('hola')
